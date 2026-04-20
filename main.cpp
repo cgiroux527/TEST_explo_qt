201ab2dff1fd5157fc44c151c820d63198be0baa
Cedric <cgiroux527@gmail.com>
2026-04-20 17:06:03 -0400

    creer une fenetre et y mettre un bouton

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QWidget>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("test bouton 1!", nullptr);
-    button.resize(100, 100);
-    button.show();
+    QWidget window;
+    window.resize(300,200);
+    window.setWindowTitle("Fenetre test");
 
-    QPushButton buttonMagique("WOW un autre bouton",nullptr);
-    buttonMagique.resize(70,80);
-    buttonMagique.show();
+    auto* boutonTest1 = new QPushButton("test bouton 1!", &window);
+    boutonTest1->move(70,70);
+    boutonTest1->show();
+    window.show();
+
+    delete boutonTest1;
     return QApplication::exec();
 }
