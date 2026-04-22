ef622649907fcbe75e21de5ab47e00bc3feed0bd
Cedric <cgiroux527@gmail.com>
2026-04-22 09:50:03 -0400

    truc de recherche

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
-#include <QApplication>
-#include <QPushButton>
-#include <QWidget>
+
+#include <QtWidgets>
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     QWidget window;
+    QLabel *etiquette = new QLabel("Recherche:");
+    QLineEdit *ligneEdition = new QLineEdit();
+    auto *boutonTest1 = new QPushButton("OK");
+
     window.resize(300,200);
     window.setWindowTitle("Fenetre test");
 
-    auto* boutonTest1 = new QPushButton("test bouton 1!", &window);
-    boutonTest1->move(70,70);
-    boutonTest1->show();
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->addWidget(etiquette);
+    layout->addWidget(ligneEdition);
+    layout->addWidget(boutonTest1);
+    window.setLayout(layout);
+
+    QObject::connect(boutonTest1, &QPushButton::clicked, [&]() {
+    etiquette->setText("...");
+});
+
     window.show();
 
-    delete boutonTest1;
     return QApplication::exec();
 }
